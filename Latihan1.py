120e1a28a99b8f54b73aff86fea57e8f7e4bdbba
Kyarawuuu <92356397+DaffaGibran3121@users.noreply.github.com>
2021-11-25 19:35:41 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,15 @@
+list=["a", "b", "c", "d", "e"]
+
+
+print("Tampilkan Element ke 3:", list[2])
+print("ambil Element ke 2 sampai 4:", list[1:4]) 
+print("ambil elemen terakhir:", list[5-1])
+
+
+# merubah elemen ke 4 dengan nilai lain
+list[3] = "f"
+
+print("merubah elemen ke 4 dengan nilai lain:", list)
+
+list[3:] = "f", "g" 
+print("merubah elemen ke 4 sampai elemen terakhir:", list)
